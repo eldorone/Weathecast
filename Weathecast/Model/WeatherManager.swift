2ee253d4faece90ce39f3db0f25161d4212339e2
Eldor Alikuvvatov <eldoralikuvvatov@gmail.com>
2022-10-08 16:55:27 +0900

    Update WeatherManager.swift

--- Weathecast/Model/WeatherManager.swift
+++ Weathecast/Model/WeatherManager.swift
@@ -37,7 +37,7 @@ struct WeatherManager {
         do {
             let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
             print(decodedData.main.temp)
-            print(weatherDescription = decodedData.weather[0].description)
+            print(decodedData.weather[0].description)
             
         } catch {
             print(error)
